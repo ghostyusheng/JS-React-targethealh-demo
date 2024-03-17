68b40f5cf6628c0c4a5864cc69a117748a6809e8
ghostyusheng <ghostyusheng@gmail.com>
2024-03-17 18:48:27 +0000

    body2 + alert

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 'use client';
 import Image from "next/image";
 import React from 'react';
-import { Button,ConfigProvider,Tabs } from 'antd';
+import { Button,ConfigProvider,Tabs,message } from 'antd';
+
+let pointer = null;
 
 const Part1 = (
   <main>
@@ -9,6 +11,22 @@ const Part1 = (
       <div className="banner-title">
         <div>TARGET HEALTHCARE LIMITED</div>
         <div className="banner-title2">Generic, branded and unlicensed pharmaceutical supplier</div>
+      </div>
+    </div>
+    <div className="body1">
+    </div>
+    <div className="ads">
+      <img className="ads-img" src="/3.png" />
+      <div>
+        <div className="ads-title1">There’s always a patient at the end of the chain.</div>
+        <div>
+          <p>Target Healthcare works with pharmacies, hospitals, dispensing doctors, pharmaceutical wholesalers, and buying groups in the UK and worldwide to deliver top-quality pharmaceutical products.</p>
+          <p>Our commitment extends beyond sourcing a wide selection of generic, branded, and licensed medicines – we’re dedicated to providing unrivalled <a title="Customer Care" href="https://target-healthcare.co.uk/customer-care/">customer service</a>.</p>
+          <p>Our mission is to support thousands of communities across the UK, Europe, and beyond, ensuring they receive the medicines they require as quickly as possible while maintaining the most competitive prices.</p>
+        </div>
+        <button className="aboutUs" onClick={() => {pointer.info("It's a React Demo")}}>
+          ABOUT US
+        </button>
       </div>
     </div>
   </main>
@@ -27,12 +45,12 @@ const items = [
   {
     key: '2',
     label: 'About Us',
-    children: 'Content of Tab Pane 2',
+    children: 'Not implement yet',
   },
   {
     key: '3',
     label: 'Specials',
-    children: 'Content of Tab Pane 3',
+    children: 'Not implement yet',
   },
   {
     key: '4',
@@ -76,6 +94,9 @@ const OperationsSlot = {
 export default function Home() {
   const [alignValue, setAlignValue] = React.useState('center');
   const [position, setPosition] = React.useState(['left', 'right']);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  pointer = messageApi;
 
   const slot = React.useMemo(() => {
     if (position.length === 0) return null;
@@ -90,6 +111,7 @@ export default function Home() {
 
   return (
     <main className="">
+      {contextHolder}
       <div className="header">
         
         <ConfigProvider 
